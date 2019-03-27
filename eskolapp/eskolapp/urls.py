187fa28d4e0c4a52ff4344e0aeeb5ce3a0ae8005
Golen <golen87@hotmail.com>
2019-03-27 16:25:44 +0100

    Repaired django project.

--- eskolapp/eskolapp/urls.py
+++ eskolapp/eskolapp/urls.py
@@ -17,11 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 
-
 from chat import views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
