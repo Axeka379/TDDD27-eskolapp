d04c240eb3927cdda7ebd917d5220bdb85f55a60
mrK <axeka379@student.liu.se>
2019-04-01 18:55:05 +0200

    Django. Added logout functionability

--- eskolapp/accounts/tests/test_form_signup.py
+++ eskolapp/accounts/tests/test_form_signup.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
-from ..forms import SignUpForm
+from ..form import SignUpForm
 
 class SignUpFormTest(TestCase):
     def test_form_has_fields(self):
         form = SignUpForm()
-        expected = ['username', 'email', 'password1', 'password2',]
+        expected = ['username', 'password1', 'password2' , 'email']
         actual = list(form.fields)
         self.assertSequenceEqual(expected, actual)
--- eskolapp/eskolapp/urls.py
+++ eskolapp/eskolapp/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from accounts import views as accounts_views
 from chat import views
@@ -23,6 +24,7 @@ from chat import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
