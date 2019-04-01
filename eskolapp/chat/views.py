6ff8b69e2abc19f0eed2177b2d387e8935c5b6c0
mrK <axeka379@student.liu.se>
2019-04-01 15:17:27 +0200

    Django. Using forms the right way and added more tests.

--- eskolapp/chat/views.py
+++ eskolapp/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .models import Board
-
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 def home(request):
     boards = Board.objects.all()
@@ -13,3 +14,23 @@ def board_topics(request, pk):
 
 def boards(request):
     return redirect(home)
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
--- eskolapp/eskolapp/urls.py
+++ eskolapp/eskolapp/urls.py
@@ -22,7 +22,6 @@ from chat import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-    #url(r'^boards/$', views.boards, name='boards'),
-
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
 ]
