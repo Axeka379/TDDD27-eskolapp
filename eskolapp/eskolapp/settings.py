f1ed1c0e0508747587f8b192432e40b7220056fe
Golen <golen87@hotmail.com>
2019-04-03 16:35:28 +0200

    Add channels-redis dependency. Add chat consumer.

--- eskolapp/eskolapp/settings.py
+++ eskolapp/eskolapp/settings.py
@@ -77,7 +77,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'eskolapp.wsgi.application'
+
+
+# Channels
+
 ASGI_APPLICATION = 'eskolapp.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
