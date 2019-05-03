047e122e48d6bbab1912598449a44830bf985fb9
mrK <axeka379@student.liu.se>
2019-05-03 16:04:47 +0200

    Added protocol for disconnect, connect and message

--- eskolapp/live/consumers.py
+++ eskolapp/live/consumers.py
@@ -1,11 +1,13 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from datetime import datetime
-import json
+import json, time
 
+# TODO:
+#   * Setup json protocol to send to frontend. (get_user?????)
+#   * Send user information that is useful.
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
@@ -23,17 +25,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             print('\033[0m')
 
         await self.accept()
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message' : '"connected to the server"',
-                'sender' : self.user.username
-            }
-
-        ) # TODO: Display the message time!
-
-
+        await self.on_join()
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -41,41 +33,81 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message' : '"disconnected from the server"',
-                'sender' : self.user.username
-            }
-        )
+        await self.on_leave()
+
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        username_str = None
-        username = self.scope['user']
-        message = text_data_json['message']
-        sender = username.username #TODO: This can be changed to id later...
-        # Send message to room group
+    async def receive_json(self, json_data):
+        method = json_data.get('type', None)
+        if method == 'message':
+            content = json_data['content']
+            await self.on_send_message(content)
+        else:
+            print("Error in receive_json:", json_data)
+
+    async def on_join(self):
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'chat_message',
-                'message': message,
-                'sender' : sender
+                'type': 'send_join',
+                'user_id' : self.user.id
             }
         )
+        await self.on_presence()
+
 
     # Receive message from room group
-    async def chat_message(self, event):
+    async def on_send_message(self, content):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'send_message',
+                'user_id' : self.user.id,
+                'content' : content,
+                'msg_id' : 1,
+                'timestamp' : time.time()
+            }
+        )
 
-        date = datetime.today().strftime('%Y-%m-%d') + ": " 
+    async def on_leave(self):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'send_leave',
+                'user_id' : self.user.id
+            }
+        )
+        await self.on_presence()
 
-        message = event['message']
-        sender = date + event['sender']
-        package = {
-            'message' : message,
-            'sender' : sender
-        }
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps(package))
+    async def on_presence(self):
+        pass
+        """
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'send_presence',
+                'user_id' : self.user.id,
+                'server' : self.room_name
+            }
+        )
+        """
+
+    async def send_join(self, event):
+        await self.send_json({
+            'type' : 'join',
+            'user_id'   : event['user_id']
+        })
+
+    async def send_message(self, event):
+        await self.send_json({
+            'type' : 'message',
+            'user_id'   : event['user_id'],
+            'msg_id' : event['msg_id'],
+            'content' : event['content'],
+            'timestamp' : event['timestamp']
+        })
+
+    async def send_leave(self, event):
+        await self.send_json({
+            'type' : 'leave',
+            'user_id'   : event['user_id']
+        })
